e32d496e34fd86d482b92c9f6af27db29a3839e9
lorehuincahue <lorehuincahue@gmail.com>
2022-09-15 00:39:10 +0000

    avance

--- src/models.py
+++ src/models.py
@@ -9,11 +9,71 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Personajes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    personajes = db.Column(db.String(30), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<Personajes %r>' % self.personajes
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "personajes": self.personajes,
+            "genero": self.genero,
+        }
+
+class Capitulos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    capitulos = db.Column(db.String(30), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<Capitulos %r>' % self.capitulos
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "capitulo": self.capitulos,
+        }
+
+class Fav_personajes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    personajes_id = db.Column(db.Integer, db.ForeignKey('personajes.id') )
+    email =  db.Column(db.String(120), db.ForeignKey('user.email'))
+    rel_user = db.relationship('User')
+    rel_pj = db.relationship('Personajes')
+
+    def __repr__(self):
+        return '<Fav_personajes %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "personajes_id": self.personajes_id,
+            "email": self.email
+        }
+
+class Fav_capitulos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    capitulos_id = db.Column(db.Integer, db.ForeignKey('capitulos.id') )
+    email =  db.Column(db.String(120), db.ForeignKey('user.email'))
+    rel_user = db.relationship('User')
+    rel_pj = db.relationship('Capitulos')
+
+    def __repr__(self):
+        return '<Fav_capitulos %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "capitulos_id": self.capitulos_id,
+            "email": self.email
         }
